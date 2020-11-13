4a2d425b2664a7815deac5fdeed7074606b340b5
Lebeil <liedel75@gmail.com>
2020-11-13 22:37:19 +0100

    add Route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from "./Components/Home";
+import Login from "./Components/auth/Login";
+import Register from "./Components/auth/Register";
+import NoPage from "./Components/NoPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/login' component={Login}/>
+                <Route exact path='/register' component={Register}/>
+                <Route component={NoPage}/>
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
